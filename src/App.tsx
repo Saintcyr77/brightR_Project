684cdceb265b598dfcc9a6dffa3d9a05d3e5adb6
Saintcyr77 <shivam.bhatt04@gmail.com>
2024-01-22 21:35:39 +0530

    started

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi optio excepturi accusamus? Sit tenetur saepe ipsum iusto earum dolorem eum consequatur ad omnis obcaecati, recusandae quisquam iste quis. Perferendis, quaerat?</p>
+       <Header/>
    </div>
     </>
   )
