dc72ef10a83567d3a3354f84a0af5037886eab34
Saintcyr77 <shivam.bhatt04@gmail.com>
2024-01-22 20:38:16 +0530

    brightR project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi optio excepturi accusamus? Sit tenetur saepe ipsum iusto earum dolorem eum consequatur ad omnis obcaecati, recusandae quisquam iste quis. Perferendis, quaerat?</p>
+   </div>
+    </>
+  )
+}
+
+export default App
