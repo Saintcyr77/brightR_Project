683129594bc5260c40e380403c051b4f9ef0fa76
Saintcyr77 <shivam.bhatt04@gmail.com>
2024-01-23 19:16:52 +0530

    header

---
+++ src/components/FirstCarousel/FirstCarousel.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './firstcar.css';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import rectangle from '../../assets/complexCarousel/Rectangle 3.png';
+
+const FirstCarousel = () => {
+  return (
+      <div>
+          <Card className='card'>
+              
+              <div className='card-body'>
+                  <div className='img'>
+                      
+                      </div>
+                  
+              </div>
+              
+  
+    </Card>
+    </div>
+  )
+}
+
+export default FirstCarousel
